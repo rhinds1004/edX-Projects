67fc7e4e7f6c0afdb66aaba165aac3ef5ef61929
robert hinds <rhinds1004@gmail.com>
2019-11-29 21:44:16 -0800

    Added properties for Uprogram name and degree. Main was changed to add the required parts for the assignment.

--- OOMod1SelfAssessment/OOMod1SelfAssessment/Program.cs
+++ OOMod1SelfAssessment/OOMod1SelfAssessment/Program.cs
@@ -11,8 +11,45 @@ namespace OOMod1SelfAssessment
         static void Main(string[] args)
         {
 
+            UProgram IntTech = new UProgram("Information Technoplogy");
+           // IntTech.ProgramName = "Information Technoplogy";
+
+            Student student1 = new Student("Autumn", "Hinds");
+            Student student2 = new Student ("Jackson", "Hinds");
+            Student student3 = new Student("Robert", "Hinds");
+
+            Course proCourse = new Course("Programming with C#");
+
+            Teacher teacher = new Teacher("No", "Name");
+
+            proCourse.AddTeacher(teacher);
+            proCourse.AddStudent(student1);
+            proCourse.AddStudent(student2);
+            proCourse.AddStudent(student3);
+
+
+            Course advProCourse = new Course("Advanced Programming with C#");
+            advProCourse.AddTeacher(teacher);
+            advProCourse.AddStudent(student1);
+            advProCourse.AddStudent(student2);
+
+
+            Degree bachOfSci = new Degree("Bachelor of Science");
+
+            bachOfSci.AddCourse(proCourse);
+            bachOfSci.AddCourse(advProCourse);
+
+            IntTech.Degree = bachOfSci;
+
+            Console.WriteLine($"Program: {IntTech.ProgramName}  Degree: {IntTech.Degree.DegreeName}");
+            foreach(Course course in IntTech.Degree.CoursesList)
+            {
+                Console.WriteLine($"Course: {course.CourseName}  student count: {course.StudentCount()}");
+            }
             
-            
+                       
+
+
 
         }
     }
---
+++ OOMod1SelfAssessment/OOMod1SelfAssessment/UProgram.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOMod1SelfAssessment
+{
+    public class UProgram
+    {
+        public Degree Degree { get; set; }
+        public string ProgramName { get; set; }
+
+        public UProgram(string programName)
+        {
+            ProgramName = programName;
+        }
+
+    }
+}
